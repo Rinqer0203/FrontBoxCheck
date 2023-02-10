78e8aa0ba4036ae621783a65792c445503966d42
Rinqer <64554381+Rinqer0203@users.noreply.github.com>
2023-02-10 12:15:21 +0900

    tagとlayerをインスペクターで設定しやすくするエディタ拡張

---
+++ UnityUtils.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TagAttribute : PropertyAttribute { }
+public class LayerAttribute : PropertyAttribute { }
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(TagAttribute))]
+public class TagDrawer : PropertyDrawer
+{
+
+	public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+	{
+
+		EditorGUI.BeginProperty(position, label, prop);
+
+		prop.stringValue = EditorGUI.TagField(position, label, prop.stringValue);
+
+		EditorGUI.EndProperty();
+	}
+}
+
+[CustomPropertyDrawer(typeof(LayerAttribute))]
+public class LayerDrawer : PropertyDrawer
+{
+
+	public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+	{
+
+		EditorGUI.BeginProperty(position, label, prop);
+
+		prop.intValue = EditorGUI.LayerField(position, label, prop.intValue);
+
+		EditorGUI.EndProperty();
+	}
+}
+#endif
